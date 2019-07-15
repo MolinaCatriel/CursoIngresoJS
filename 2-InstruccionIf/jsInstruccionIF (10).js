2bf73c9687f07382135aafa38d3bb73bcc986ee5
MolinaCatriel <Catrielmolina.cm@gmail.com>
2019-07-15 09:53:45 -0300

    LUNES 15 9:53

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,12 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var nota;
+ if(nota=Math.floor (Math.random() *10) +1)
+	{
+		alert("EXCELENTE");
+	}
+
 	
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,22 +1,16 @@
 function mostrar()
 {
 //tomo la edad  
-var edad;
+    var edad;
     var estadocivil;
     
     edad=document.getElementById("edad").value;
 
     estadocivil=document.getElementById("estadoCivil").value;
 
-    if(edad>=18 && estadocivil!="Soltero")   
+    if(edad>=18 && estadocivil=="Soltero")   
     {
-
-    
-
-    {
-        alert("Es soltero y no es menor.");
-    }
-
+        alert("Es soltero y no es menor");
     }
 	
 
