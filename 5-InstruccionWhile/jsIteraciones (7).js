14b83b4545652df0633bc7eb73c5e3031facb911
MolinaCatriel <Catrielmolina.cm@gmail.com>
2019-08-01 21:01:47 -0300

    Hasta ejercicio 8 WHILE

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,12 +1,26 @@
 function mostrar()
 {
 
-	var contador=0;
-	var acumulador=0;
-	var respuesta='si';
+	var contador = 0;
+	var acumulador = 0;
+	var numero;
+	var promedio
+	var seguir = 's';
 
+	do {
+		numero = parseInt(prompt("Ingrese un numero"));
+		while(isNaN(numero))
+		{
+			numero=prompt("Error, ingrese un numero");
+		}
+		contador++;
+		acumulador = acumulador + numero;
+		seguir = prompt("¿Quiere seguir? s: para si n: para no.");
+	} while (seguir == 's');
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/contador;
+	promedio = acumulador / numero;
+
+	document.getElementById('suma').value = acumulador;
+	document.getElementById('promedio').value = promedio;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,14 +1,36 @@
 function mostrar()
 {
+	var contNegativo = 0;
+	var positivo = 0;
+	var negativo = 1;
+	var seguir = 's';
+	var numero;
 
-	var contador=0;
-	var positivo=0;
-	var negativo=1;
-	
-	var respuesta='si';
+	do {
+		numero = parseInt(prompt("Ingrese un numero"));
+		while (isNaN(numero)) {
+			numero = prompt("Error, ingrese un numero");
+		}
 
 
-document.getElementById('suma').value=positivo;
-document.getElementById('producto').value=negativo;
+		if (numero < 0) {
+			negativo = negativo * numero;
+			contNegativo++;
+		}
+		else {
+			positivo = positivo + numero;
+
+		}
+		seguir = prompt("¿Quiere seguir? s: para si n: para no.");
+	}
+	while (seguir == 's');
+	if (contNegativo == 0) {
+		negativo = 0
+	}
+
+
+
+	document.getElementById('suma').value = positivo;
+	document.getElementById('producto').value = negativo;
 
 }//FIN DE LA FUNCIÓN
